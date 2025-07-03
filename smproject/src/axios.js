e60800c4c03341ffd4be06b50e3424167f950996
Shrihari <140818388+shrihariss29@users.noreply.github.com>
2025-07-03 22:00:01 +0530

    Update axios.js

--- smproject/src/axios.js
+++ smproject/src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL:"http://ec2-184-73-128-194.compute-1.amazonaws.com"
+    baseURL:"https://ec2-184-73-128-194.compute-1.amazonaws.com"
 })
 
 export default instance;
